44042430e486569a59ca00d82ed8357ec4aa1301
Gustavo <gusstavocta@gmail.com>
2024-09-06 00:11:27 -0500

    implementando redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { PokemonApp } from './store/PokemonApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <Provider store={store}>
 
-    <App />
+    {/* <App /> */}
+    <PokemonApp />
     </Provider>
   </StrictMode>,
 )
---
+++ src/store/PokemonApp.jsx
@@ -0,0 +1,16 @@
+
+
+export const PokemonApp = () => {
+  return (
+    <>
+        <h1>Pokemon App</h1>
+        <hr />
+        <ul>
+            <li>Pikachu</li>
+            <li>Bulbasaur</li>
+            <li>Charmander</li>
+            <li>Squirtle</li>
+        </ul>
+    </>
+  )
+}
